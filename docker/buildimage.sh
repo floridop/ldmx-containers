e4b927e4acaf320af19d7d66fd14441836d2f1aa
Florido Paganelli <florido.paganelli@hep.lu.se>
2019-06-25 14:23:06 +0200

    Main buildimage script with all image steps

--- docker/buildimage.sh
+++ docker/buildimage.sh
@@ -2,5 +2,7 @@
 
 docker build -t ldmx:centosbase -f /export/hepdrift/dockerstuff/ldmx/Dockerfile0.centosbase .
 docker build -t ldmx:0.1 -f /export/hepdrift/dockerstuff/ldmx/Dockerfile1.xerces .
-docker build -t ldmx:0.2 -f /export/hepdrift/dockerstuff/ldmx/Dockerfile2.geant4 .
+docker build -t ldmx:0.2 -f /export/hepdrift/dockerstuff/ldmx/Dockerfile2.Geant4 .
 docker build -t ldmx:0.3 -f /export/hepdrift/dockerstuff/ldmx/Dockerfile3.root .
+docker build -t ldmx:0.4 -f /export/hepdrift/dockerstuff/ldmx/Dockerfile4.ldmx-sw .
+
