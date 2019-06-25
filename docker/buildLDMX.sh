eb5d28517afbb528b4e653990f9b58882e0ce107
Florido Paganelli <florido.paganelli@hep.lu.se>
2019-06-25 14:22:05 +0200

    Fixed broken G4 dir -- cmake does not scan subdirs

--- docker/buildLDMX.sh
+++ docker/buildLDMX.sh
@@ -3,5 +3,5 @@
 git clone https://github.com/LDMXAnalysis/ldmx-sw.git
 cd ldmx-sw
 mkdir build; cd build
-cmake -DGeant4_DIR=$G4DIR -DROOT_DIR=$ROOTDIR -DXercesC_DIR=$XercesC_DIR -DCMAKE_INSTALL_PREFIX=../ldmx-sw-install ..
+cmake -DGeant4_DIR=$G4DIR/lib64/Geant4-10.2.3/ -DROOT_DIR=$ROOTDIR -DXercesC_DIR=$XercesC_DIR/install -DCMAKE_INSTALL_PREFIX=../ldmx-sw-install ..
 make install
