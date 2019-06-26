54f077a81affa52eea34c5f47c396691428261d4
Florido Paganelli <florido.paganelli@hep.lu.se>
2019-06-26 17:34:35 +0200

    removed hardcoded paths from buildimage, added gcc support

--- docker/buildimage.sh
+++ docker/buildimage.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-docker build -t ldmx:centosbase -f /export/hepdrift/dockerstuff/ldmx/Dockerfile0.centosbase .
-docker build -t ldmx:0.1 -f /export/hepdrift/dockerstuff/ldmx/Dockerfile1.xerces .
-docker build -t ldmx:0.2 -f /export/hepdrift/dockerstuff/ldmx/Dockerfile2.Geant4 .
-docker build -t ldmx:0.3 -f /export/hepdrift/dockerstuff/ldmx/Dockerfile3.root .
-docker build -t ldmx:0.4 -f /export/hepdrift/dockerstuff/ldmx/Dockerfile4.ldmx-sw .
+docker build -t ldmx:centosbase -f Dockerfile0.centosbase .
+docker build -t ldmx:centosbase.gcc -f Dockerfile0.centosbase.gcc .
+docker build -t ldmx:0.1 -f Dockerfile1.xerces .
+docker build -t ldmx:0.2 -f Dockerfile2.Geant4 .
+docker build -t ldmx:0.3 -f Dockerfile3.root .
+docker build -t ldmx:0.4 -f Dockerfile4.ldmx-sw .
 
