9af74f244162c51fc999fa40779fb91172e99fd3
Florido Paganelli <florido.paganelli@hep.lu.se>
2019-06-27 18:18:26 +0200

    tweaks for cmake to detect PYTHON_EXECUTABLE, added vim for convenience

--- docker/buildLDMX.sh
+++ docker/buildLDMX.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+PYTHON_EXECUTABLE=`which python`
+
 mkdir -p /ldmx/build
 cd /ldmx/build
 git clone https://github.com/LDMXAnalysis/ldmx-sw.git
 cd ldmx-sw
 mkdir build; cd build
-cmake -DGeant4_DIR=$G4DIR/lib64/Geant4-10.2.3/ -DROOT_DIR=$ROOTDIR -DXercesC_DIR=$XercesC_DIR -DCMAKE_INSTALL_PREFIX=/ldmx/libs/ldmx-sw-install ..
+cmake -DPYTHON_EXECUTABLE=/usr/bin/python -DGeant4_DIR=$G4DIR/lib64/Geant4-10.2.3/ -DROOT_DIR=$ROOTDIR -DXercesC_DIR=$XercesC_DIR -DCMAKE_INSTALL_PREFIX=/ldmx/libs/ldmx-sw-install ..
 make install
