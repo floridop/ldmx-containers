c8428d780c8efe15c3aa58bd608e6c745c00de43
Florido Paganelli <florido.paganelli@hep.lu.se>
2019-06-27 13:40:53 +0200

    fixed typo in library folder name

--- docker/buildGeant4.sh
+++ docker/buildGeant4.sh
@@ -11,9 +11,9 @@ cd geant4
 git checkout tags/${LDMXTAGS} -b ${LDMXTAGS}
 mkdir build; cd build
 cmake -DGEANT4_USE_GDML=ON -DGEANT4_INSTALL_DATA=ON -DXERCESC_ROOT_DIR=${XercesC_DIR} \
-    -DGEANT4_USE_OPENGL_X11=ON -DCMAKE_INSTALL_PREFIX=/ldxm/libs/geant4 -DGEANT4_USE_SYSTEM_EXPAT=OFF ..
+    -DGEANT4_USE_OPENGL_X11=ON -DCMAKE_INSTALL_PREFIX=/ldmx/libs/geant4 -DGEANT4_USE_SYSTEM_EXPAT=OFF ..
 make install
-cd /ldxm/libs/geant4
+cd /ldmx/libs/geant4
 export G4DIR=$PWD
 
 
