717fcdd67282a7f9bd8d200ea82e8f3a4bab9b3e
Florido Paganelli <florido.paganelli@hep.lu.se>
2019-06-28 15:42:52 +0200

    Including python
    - python installation is performed after gcc, some packages required pip.
    - step 1 xerces is built on top of an existing python installation
    - the buildimage.sh script has been updated to match the changes

--- docker/buildimage.sh
+++ docker/buildimage.sh
@@ -5,8 +5,10 @@
 
 docker build -t ldmx:centosbase -f Dockerfile0.centosbase .
 docker build -t ldmx:centosbase.gcc -f Dockerfile0.centosbase.gcc .
+docker build -t ldmx:centosbase.python -f Dockerfile0.centosbase.python .
 docker build -t ldmx:0.1 -f Dockerfile1.xerces .
 docker build -t ldmx:0.2 -f Dockerfile2.root .
 docker build -t ldmx:0.3 -f Dockerfile3.Geant4 .
 docker build -t ldmx:0.4 -f Dockerfile4.ldmx-sw .
+docker build -t ldmx:tests -f Dockerfile5.tests .
 
